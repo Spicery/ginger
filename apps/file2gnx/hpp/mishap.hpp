415bac76965b80f4bc5a646411fd7c3d330d2b8b
sfkleach <sfkleach@4b70db30-0924-4863-9dc2-3f727e8e7b6d>
2010-12-02 20:20:50 +0000

    Problems with repo

--- apps/file2gnx/hpp/mishap.hpp
+++ apps/file2gnx/hpp/mishap.hpp
@@ -16,6 +16,7 @@
     along with AppGinger.  If not, see <http://www.gnu.org/licenses/>.
 \******************************************************************************/
 
+
 #ifndef MISHAP_HPP
 #define MISHAP_HPP
 

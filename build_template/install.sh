95bcc1ac859d0f71ad02ffda10c9baeb489af151
Stephen Leach <sfkleach@gmail.com>
2011-11-04 14:04:56 +0000

    Required for the snapshot builder.

---
+++ build_template/install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#
+#	Install the packed files archive into /usr/local. We assume that the
+#	packed files archive is in the current directory. 
+#	N.B. You will probably need to run this script with root privileges.
+#
+PACKED_FILES=appginger-packed-files.tgz
+INSTALL_DIR=/usr/local
+gzip -d $PACKED_FILES | ( cd $INSTALL_DIR; tar zxf -)

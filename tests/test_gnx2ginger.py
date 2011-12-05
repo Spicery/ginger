35b2b9ed269b2963fe1aa9899117d5cb07cd1387
Graham Higgins <gjh@bel-epa.com>
2011-12-05 23:47:32 +0000

    Setting gnx2ginger test output string matches.

--- tests/test_gnx2ginger.py
+++ tests/test_gnx2ginger.py
@@ -1,6 +1,7 @@
 import unittest
 import os
 import commands
+
 # import textwrap
 # from tempfile import mkdtemp
 
@@ -15,20 +16,19 @@ class GNX2GingerTestCase(unittest.TestCase):
         """gnx2ginger - print"""
         fn = "print"
         res = commands.getoutput("""cat %s | appginger""" % (self.infn % fn))
-        print("Res %s" % res)
-        self.assert_(res == None)
+        # print("Res %s" % res)
+        self.assert_("""Problem: Cannot find variable\nVariable : _print""" in res)
 
     def test_gnx2ginger_nfib(self):
         """gnx2ginger - nfib"""
         fn = "nfib"
         res = commands.getoutput("""cat %s | appginger""" % (self.infn % fn))
-        print("Res %s" % res)
-        self.assert_(res == None)
+        # print("Res: %s" % res)
+        self.assert_("There is 1 result" in res and "1.\t18454929" in res)
 
     def test_gnx2ginger_factorial(self):
         """gnx2ginger - factorial"""
         fn = "factorial"
         res = commands.getoutput("""cat %s | appginger""" % (self.infn % fn))
-        print("Res %s" % res)
-        self.assert_(res == None)
-
+        # print("Res %s" % res)
+        self.assert_("There is 1 result" in res and "1.\t6" in res)

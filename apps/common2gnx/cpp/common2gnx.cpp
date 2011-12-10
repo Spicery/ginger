6c89454b29570786991361b1addf3e2df7d9cae1
Stephen Leach <sfkleach@gmail.com>
2011-12-10 00:07:36 +0000

    Added missing copyright

--- apps/common2gnx/cpp/common2gnx.cpp
+++ apps/common2gnx/cpp/common2gnx.cpp
@@ -1,3 +1,21 @@
+/******************************************************************************\
+    Copyright (c) 2010 Stephen Leach. AppGinger is distributed under the terms 
+    of the GNU General Public License. This file is part of AppGinger.
+
+    AppGinger is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    AppGinger is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with AppGinger.  If not, see <http://www.gnu.org/licenses/>.
+\******************************************************************************/
+
 #include <iostream>
 #include <fstream>
 
@@ -257,7 +275,12 @@ int main( int argc, char **argv, char **envp ) {
 		main.parseArgs( argc, argv, envp );
 		return main.run();
 	} catch ( Ginger::Mishap & p ) {
-		p.report();
+		p.culprit( "Detected by", APP_TITLE );
+		p.gnxReport();
+		return EXIT_FAILURE;
+	} catch ( Ginger::Mishap & p ) {
+		p.culprit( "Detected by", APP_TITLE );
+		p.gnxReport();
 		return EXIT_FAILURE;
 	}
 }

298dd4fabeaa3dcd171daa34acddb08f2bb6e674
Stephen Leach <sfkleach@gmail.com>
2012-10-24 23:18:31 +0100

    Now using Python only & ready to deprecate Java.

--- apps/automatic/metainfo/make_metainfo.py
+++ apps/automatic/metainfo/make_metainfo.py
@@ -1,22 +1,47 @@
+#!/usr/bin/python
+
 import subprocess
 import json
 import os
+
+
+################################################################################
+#   Arity
+################################################################################
+
+class Arity:
+	
+	def __init__( self, arity ):
+		self.count = int( arity.replace( "+", "" ) )
+		self.more = arity.find( "+" ) != -1
+		
+	def __str__( self ):
+		m = str( self.more ).lower()
+		return "Arity( {}, {} )".format( self.count, m )
+
+
+
 
 ################################################################################
 #   generateMetaInfoTable
 ################################################################################
 
-def metaInfoDir():
-	"""Returns the directory for the metainfo"""
-	# For development we use tmp_metainfo rather than metainfo.
-	return "../automatic/tmp_metainfo"
-
-def generateMetaInfo( ):
+def generateMetaInfo():
 	metainfo = json.loads( subprocess.check_output( [ "../../appginger/cpp/ginger-info", "-j" ] ) )
 	stdinfo = metainfo[ "std" ]
-	dir = metaInfoDir()
-	if not os.path.exists( dir ):
-		os.makedirs( dir )	
-	return
+
+	pw = open( "metainfo.cpp.inc", 'w' )
+   	
+   	for name in stdinfo:
+   		e = stdinfo[ name ]
+		pw.write(
+			"MAPLET( \"{}\", MetaInfo( {}, {} ) ),\n".format(
+				name, 
+				Arity( e[ "in" ] ), 
+				Arity( e[ "out" ] )
+			)
+		)
+	pw.close()
+
 
 generateMetaInfo()

9284d3066a154a54447b1ca3cdadb289a5971e1f
Stephen Leach <sfkleach@gmail.com>
2012-12-19 15:41:47 +0000

    Revised the nosetests for common2gnx so as to use the recently built executable.

--- functests/test_cmn2gnx.py
+++ functests/test_cmn2gnx.py
@@ -4,66 +4,69 @@ import commands
 # import textwrap
 # from tempfile import mkdtemp
 
-cmn2gnx = os.path.dirname(os.path.realpath( __file__ )) + "/cmn2gnx/"
+here = os.path.dirname( os.path.realpath( __file__ ) )
+home = os.path.join( here, ".." )
+cmn2gnx = os.path.join( here, "cmn2gnx" )
+executable = os.path.join( home, "apps/common2gnx/cpp/common2gnx")
 
 class Common2GNXTestCase(unittest.TestCase):
 
     def setUp(self):
-        self.infn = cmn2gnx + "%s.cmn"
-        self.outfn = cmn2gnx  + "%s.gnx"
+        self.infn = cmn2gnx + "/%s.cmn"
+        self.outfn = cmn2gnx  + "/%s.gnx"
 
     def test_common2gnx_print_define(self):
         """common2gnx - simple print define"""
         fn = "print_define"
-        res = commands.getoutput('cat %s | common2gnx' % (self.infn % fn))
+        res = commands.getoutput( "cat {0} | {1} -s".format( (self.infn % fn), executable ) )
         # print("Res %s" % res)
-        self.assert_(res == open(self.outfn % fn).read())
+        self.assertEqual(res, open(self.outfn % fn).read())
 
     def test_common2gnx_print_fncall(self):
         """common2gnx - print fncall"""
         fn = 'print_fncall'
-        res = commands.getoutput('cat %s | common2gnx' % (self.infn % fn))
+        res = commands.getoutput( "cat {0} | {1} -s".format( (self.infn % fn), executable ) )
         # print("Res %s" % res)
-        self.assert_(res == open(self.outfn % fn).read())
+        self.assertEqual(res, open(self.outfn % fn).read())
 
     def test_common2gnx_nfib_define(self):
         """common2gnx - nfib define"""
         fn = "nfib_define"
-        res = commands.getoutput('cat %s | common2gnx' % (self.infn % fn))
+        res = commands.getoutput( "cat {0} | {1} -s".format( (self.infn % fn), executable ) )
         # print("Res %s" % res)
-        self.assert_(res == open(self.outfn % fn).read())
+        self.assertEqual(res, open(self.outfn % fn).read())
 
     def test_common2gnx_nfib_fncall(self):
         """common2gnx - nfib fncall"""
         fn = 'nfib_fncall'
-        res = commands.getoutput('cat %s | common2gnx' % (self.infn % fn))
+        res = commands.getoutput( "cat {0} | {1} -s".format( (self.infn % fn), executable ) )
         # print("Res %s" % res)
-        self.assert_(res == open(self.outfn % fn).read())
+        self.assertEqual(res, open(self.outfn % fn).read())
 
     def test_common2gnx_factorial_define(self):
         """common2gnx - factorial define"""
         fn = "factorial_define"
-        res = commands.getoutput('cat %s | common2gnx' % (self.infn % fn))
+        res = commands.getoutput( "cat {0} | {1} -s".format( (self.infn % fn), executable ) )
         # print("Res %s" % res)
-        self.assert_(res == open(self.outfn % fn).read())
+        self.assertEqual(res, open(self.outfn % fn).read())
 
     def test_common2gnx_factorial_fncall(self):
         """common2gnx - factorial fncall"""
         fn = 'factorial_fncall'
-        res = commands.getoutput('cat %s | common2gnx' % (self.infn % fn))
+        res = commands.getoutput( "cat {0} | {1} -s".format( (self.infn % fn), executable ) )
         # print("Res %s" % res)
-        self.assert_(res == open(self.outfn % fn).read())
+        self.assertEqual(res, open(self.outfn % fn).read())
 
     def test_common2gnx_hello_define(self):
         """common2gnx - hello define - multiple definitions in source"""
         fn = "hello_define"
-        res = commands.getoutput('cat %s | common2gnx' % (self.infn % fn))
+        res = commands.getoutput( "cat {0} | {1} -s".format( (self.infn % fn), executable ) )
         # print("Res %s" % res)
-        self.assert_(res == open(self.outfn % fn).read())
+        self.assertEqual(res, open(self.outfn % fn).read())
 
     def test_common2gnx_hello_fncall(self):
         """common2gnx - hello fncall"""
         fn = 'hello_fncall'
-        res = commands.getoutput('cat %s | common2gnx' % (self.infn % fn))
+        res = commands.getoutput( "cat {0} | {1} -s".format( (self.infn % fn), executable ) )
         # print("Res %s" % res)
         self.assert_(res == open(self.outfn % fn).read())

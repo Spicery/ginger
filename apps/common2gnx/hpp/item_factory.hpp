58ace277b4335055aeb332f8708ed33661371e07
Stephen Leach <sfkleach@gmail.com>
2012-12-26 19:46:18 +0000

    Tidied up old commented-out code in item_factory.hpp

--- apps/common2gnx/hpp/item_factory.hpp
+++ apps/common2gnx/hpp/item_factory.hpp
@@ -33,7 +33,6 @@ class ItemFactoryClass : public Source {
 public:
 	bool				cstyle_mode;
 	ItemMap				itemMap;
-	//FILE				*file;
     bool            	peeked;
     std::string			text;
 	Item				item;
@@ -55,11 +54,9 @@ private:
 
 public:
 	Item read();
-	//std::string readURL();
 	void unread();
 	void drop();
 	Item peek();
-	//int peekchar();
 	void reset();
 	
 public:
@@ -81,12 +78,6 @@ public:
 };
 
 typedef class ItemFactoryClass *ItemFactory;
-
-//Item item_factory_read( ItemFactory tok );
-//void item_factory_unread( ItemFactory tok );
-//Item item_factory_peek( ItemFactory tok );
-//void item_factory_drop( ItemFactory tok );
-//void item_factory_reset( ItemFactory tok );
 
 #endif
 

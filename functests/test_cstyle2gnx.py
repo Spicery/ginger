18d979d9fcc40a8ca1bb7bd077bad878d5af4725
Stephen Leach <sfkleach@gmail.com>
2012-12-26 00:51:36 +0000

    Adding tests for the cstyle syntax.

--- functests/test_cstyle2gnx.py
+++ functests/test_cstyle2gnx.py
@@ -8,10 +8,14 @@ here = os.path.dirname( os.path.realpath( __file__ ) )
 home = os.path.join( here, ".." )
 executable = os.path.join( home, "apps/common2gnx/cpp/cstyle2gnx")
 
+def trimMnxString( m ):
+	m = re.sub( r'\>\s+\<', '><', m )
+	return re.sub( r'\s\s+', ' ', m )
+
 def runtest( in_file_name ):
-    out_file_name = re.sub( r'\.in.cst$', '.out.gnx', in_file_name )
+    out_file_name = re.sub( r'\.in\.cst$', r'.out.gnx', in_file_name )
     res = commands.getoutput( "cat {0} | {1} -s".format( in_file_name, executable ) )
-    nose.tools.assert_equal( res, open( out_file_name ).read().strip() )
+    nose.tools.assert_equal( trimMnxString( res ), trimMnxString( open( out_file_name ).read().strip() ) )
 
 def test_examples():
     for in_file_name in glob.glob( "cstyle2gnx/*.in.cst" ):

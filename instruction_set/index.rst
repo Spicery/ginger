fdf033a2b3eaa42ef6f21c8849261808a192ef6f
Stephen Leach <sfkleach@gmail.com>
2013-02-09 12:35:25 +0000

    Putting the instruction set documentation builder under version control. In conversation with Graham Higgins, he recommended that we shift this to gingerdocs. I've added a task to Trello to reflect this.

---
+++ instruction_set/index.rst
@@ -0,0 +1,23 @@
+.. Ginger Instruction Set documentation master file, created by
+   sphinx-quickstart on Sun Jan 27 21:23:33 2013.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to Ginger Instruction Set's documentation!
+==================================================
+
+Contents:
+
+.. toctree::
+   :maxdepth: 2
+
+   Instruction Set <instructions>
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
+
---
+++ instruction_set/make_instrsetdocs.py
@@ -0,0 +1,65 @@
+#!/usr/bin/python
+
+import glob
+import re
+
+line_a = "/*****************************************************************************\\\n"
+line_b = "\\*****************************************************************************/\n"
+
+def code_header():
+	print
+	print ".. code-block:: c++"
+	print
+
+def code_footer():
+	print
+
+def extractDocs( file ):
+	state = 0
+	for line in file:
+		#print( "> ", line )
+		if line == line_a:
+			state = 1
+		elif line == line_b:
+			state = 2
+			code_header()
+		elif state == 1:
+			if line[ 0 ].isalpha():
+				print "**{}**".format( line.rstrip() )
+			else:
+				print line,
+		elif state == 2:
+			print "    ", line,
+	if state == 2:
+		code_footer()
+
+def file_header( fname ):
+	print ".. Start of file", fname
+	print
+	m = re.match( "([^.]*)", fname )
+	title = "Instruction {}".format( m.group( 1 ) )
+	print title
+	print "-" * len( title )
+	print
+
+def file_footer( fname ):
+	print ".. End of file", fname
+	print
+
+def doc_header():
+	print "======================"
+	print "Ginger Instruction Set"
+	print "======================"
+	print
+
+def doc_footer():
+	print ".. End of document"
+
+def processAllFiles():
+	doc_header()
+	for f in glob.glob( "*.i*" ):
+		file_header( f )
+		extractDocs( open( f ) )
+		file_footer( f )
+	doc_footer()
+processAllFiles()

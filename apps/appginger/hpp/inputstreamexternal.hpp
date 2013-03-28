b2eab375764bc485322d1488cedb9505e54b949c
Stephen Leach <sfkleach@gmail.com>
2013-03-28 13:20:53 +0000

    Files needed for external objects.

---
+++ apps/appginger/hpp/inputstreamexternal.hpp
@@ -0,0 +1,49 @@
+/******************************************************************************\
+	Copyright (c) 2010 Stephen Leach. AppGinger is distributed under the terms 
+	of the GNU General Public License. This file is part of AppGinger.
+
+    AppGinger is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    AppGinger is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with AppGinger.  If not, see <http://www.gnu.org/licenses/>.
+\******************************************************************************/
+
+#ifndef GNG_INPUT_STREAM_HPP
+#define GNG_INPUT_STREAM_HPP
+
+#include <memory>
+#include <istream>
+#include <string>
+
+#include "external.hpp"
+
+namespace Ginger {
+
+class InputStreamExternal : public External {
+private:
+    std::string file_name; 
+    std::auto_ptr< std::istream > input;
+
+public:
+    InputStreamExternal( std::string & _fname );
+    virtual ~InputStreamExternal();
+
+public:
+    void print( std::ostream & out );
+    Ref * sysApply( Ref * pc, MachineClass * vm );
+
+public:
+    bool isGood() const;
+};
+
+} // namespace Ginger
+
+#endif

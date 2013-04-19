aea8a13a3e2bc5abf3918722bcbfcb53acd6f3f0
Stephen Leach <sfkleach@gmail.com>
2013-04-19 09:28:50 +0100

    Adds building the Ginger documentation set and the distribution tarball.

--- design/bootstrap.sh
+++ design/bootstrap.sh
@@ -1,5 +1,11 @@
+###############################################################################
+# Install the dependencies for building Ginger.
+###############################################################################
+
 sudo apt-get update
 sudo apt-get install -y make git python g++ autoconf uuid-dev libreadline-dev
+
+# RudeCGI is part of the Ginger dependencies that needs separate download.
 
 mkdir /tmp/rudecgi
 cd /tmp/rudecgi
@@ -10,7 +16,14 @@ cd rudecgi-5.0.0
 make
 sudo make install
 
-sudo apt-get install rlwrap
+# Now install the pre-requisites for the Ginger runtime.
+
+sudo apt-get install -y rlwrap
+
+###############################################################################
+# And then build Ginger from the GitHub repo.
+###############################################################################
+
 SPICERY_HOME=~/Spicery
 mkdir $SPICERY_HOME
 cd $SPICERY_HOME
@@ -19,3 +32,27 @@ git clone https://github.com/Spicery/ginger.git
 cd ginger
 autoconf; ./configure; make
 sudo make install-as-is
+
+###############################################################################
+# Followed by building the Gingerdocs
+###############################################################################
+
+cd $SPICERY_HOME
+git clone https://github.com/Spicery/gingerdocs.git
+cd gingerdocs
+make html
+make install-as-is
+
+###############################################################################
+# And finally constructing the tarball
+###############################################################################
+
+cd $SPICERY_HOME
+cd ginger
+make -f MakeTarball tarball-as-is
+
+# Copying back out.
+cp _build/ginger.tgz /vagrant
+
+
+###############################################################################

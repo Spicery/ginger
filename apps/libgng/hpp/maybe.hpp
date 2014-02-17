9d43a8a9c849c27d74a39e3c567b67a7faa0f7cc
Stephen Leach <sfkleach@gmail.com>
2014-02-17 08:36:51 +0000

    Added missing commit of 'maybe' implementation to mnx.cpp

---
+++ apps/libgng/hpp/maybe.hpp
@@ -0,0 +1,44 @@
+/******************************************************************************\
+    Copyright (c) 2010 Stephen Leach. AppGinger is distributed under the terms 
+    of the GNU General Public License. This file is part of AppGinger.
+
+    AppGinger is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    AppGinger is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with AppGinger.  If not, see <http://www.gnu.org/licenses/>.
+\******************************************************************************/
+
+#ifndef GINGER_MAYBE_HPP
+#define GINGER_MAYBE_HPP
+
+namespace Ginger {
+
+template< typename T > 
+class Maybe {
+private:
+    T 		a_value;
+    bool 	is_valid;
+
+public:
+    Maybe( const T& x ) : a_value( x ), is_valid( true ) {}
+    Maybe() : is_valid( false ) {}
+
+    bool isValid() const { return this->is_valid; }
+
+    T& fastValue() { return this->a_value; }
+
+    T& value();
+
+};
+
+} // namespace
+
+#endif

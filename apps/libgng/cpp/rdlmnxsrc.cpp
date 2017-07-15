9d137741af84540ca93c32fec12e4a2d076688ab
Stephen Leach <sfkleach@gmail.com>
2017-07-15 13:27:00 +0100

    Replaced AppGinger with Ginger in copyright messages + a handful of file ending normalisations.

--- apps/libgng/cpp/rdlmnxsrc.cpp
+++ apps/libgng/cpp/rdlmnxsrc.cpp
@@ -1,19 +1,19 @@
 /******************************************************************************\
     Copyright (c) 2010 Stephen Leach. AppGinger is distributed under the terms 
-    of the GNU General Public License. This file is part of AppGinger.
+    of the GNU General Public License. This file is part of Ginger.
 
-    AppGinger is free software: you can redistribute it and/or modify
+    Ginger is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
     the Free Software Foundation, either version 3 of the License, or
     (at your option) any later version.
 
-    AppGinger is distributed in the hope that it will be useful,
+    Ginger is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
     GNU General Public License for more details.
 
     You should have received a copy of the GNU General Public License
-    along with AppGinger.  If not, see <http://www.gnu.org/licenses/>.
+    along with Ginger.  If not, see <http://www.gnu.org/licenses/>.
 \******************************************************************************/
 
 #include <stdio.h>

e96b5fda21c62669820498fcb40bd02fb596a5cd
Stephen Leach <sfkleach@gmail.com>
2017-07-16 09:20:29 +0100

    Fix warning by following the advice in the message -
    WARNING: sphinx.ext.pngmath has been deprecated. Please use sphinx.ext.imgmath instead.

--- design/conf.py
+++ design/conf.py
@@ -33,7 +33,7 @@ import cloud_sptheme as csp
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'sphinx.ext.pngmath',
+    'sphinx.ext.imgmath',
 ]
 
 # Add any paths that contain templates here, relative to this directory.

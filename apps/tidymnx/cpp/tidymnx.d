c7f8e77633d0f77d6be1e1dde3b2f9edc4659dd4
Stephen Leach <sfkleach@gmail.com>
2017-07-19 17:40:51 +0100

    Comitted in error.

--- apps/tidymnx/cpp/tidymnx.d
+++
@@ -1,4 +0,0 @@
-tidymnx.o: tidymnx.cpp ../../libgng/hpp/printgpl.hpp \
-  ../../libgng/hpp/gngversion.hpp ../../libgng/hpp/config.h \
-  ../../libgng/hpp/mnx.hpp ../../libgng/hpp/shared.hpp \
-  ../../libgng/hpp/maybe.hpp ../../libgng/hpp/mishap.hpp

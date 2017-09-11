6646fd507ce781489225b3be12fe4279e1fe17c2
Stephen Leach <sfkleach@gmail.com>
2017-09-12 00:12:17 +0100

    Fix error in using nargs rather than nlocals.

--- apps/fn2code/prototype/fn2code.py
+++ apps/fn2code/prototype/fn2code.py
@@ -18,9 +18,10 @@ def transformFn( fn ):
     args = fn[0]
     body = fn[1]
     nargs = int( fn.get( "args.count" ) )
+    nlocals = int( fn.get( "locals.count" ) )
     fn2 = MinXML( "fn.code", **fn.attributes )
-    ( cbody, max_slots ) = compiler.compile( body, nargs )
-    fn2.children = [ enterInstruction( nargs ) ]
+    ( cbody, max_slots ) = compiler.compile( body, nargs, nlocals )
+    fn2.children = [ enterInstruction( nlocals ) ]
     fn2.children.extend( backend.backEnd( cbody.getChildren() ) )
     fn2.put( "locals.count", str( max_slots ) )
     return fn2
@@ -40,12 +41,15 @@ def main():
         gnx = readMinXML( sys.stdin )
         if gnx == None:
             break
-        print( "Received: ", file=sys.stderr )
-        gnx.pretty( file=sys.stderr, indent=1 )
-        gnx = transform( gnx )
-        print( "Generated: ", file=sys.stderr )
-        gnx.pretty( file=sys.stderr, indent=1 )
-        print( gnx )
+        if gnx.hasName( 'seq' ) and not( gnx ):
+            print( gnx )
+        else:
+            print( "Received: ", file=sys.stderr )
+            gnx.pretty( file=sys.stderr, indent=1 )
+            gnx = transform( gnx )
+            print( "Generated: ", file=sys.stderr )
+            gnx.pretty( file=sys.stderr, indent=1 )
+            print( gnx )
         sys.stdout.flush()  # Required to ensure proper switch of control between processes.
 
 if __name__ == "__main__":
